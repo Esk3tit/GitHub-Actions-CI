840a35a88461b593db2e89b9d2f19d0901a5d31d
Kai Pan <khaiphn41@gmail.com>
2021-06-03 05:56:37 -0700

    added code and tests for multiplication functionality.

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ class TestCase(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(example.subtract(1, 1), 0)
 
+    def test_multiply(self):
+        self.assertEqual(example.multiply(1, 1), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
